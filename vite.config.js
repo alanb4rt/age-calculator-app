654a9460676017081cef71529c8e3f45a39bbbba
alanb4rt <al4n.b4rt@gmail.com>
2024-09-16 11:14:38 +0200

    fix base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "age-calculator-app",
+  base: "/age-calculator-app/",
   plugins: [react()],
 })
